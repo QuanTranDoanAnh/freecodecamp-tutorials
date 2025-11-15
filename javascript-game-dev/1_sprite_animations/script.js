4e2b08e93d1c0128ddad94c4059442127fb6e8ee
QuanTranDoanAnh <td.anhquan@gmail.com>
2025-11-15 20:54:23 +0700

    1st methods for sprite animations- using gameFrame and staggerFrame

--- javascript-game-dev/1_sprite_animations/script.js
+++ javascript-game-dev/1_sprite_animations/script.js
@@ -1,3 +1,27 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png'; // Ensure this image is in the same directory or provide the correct path
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(x, 50, 100, 100); // Draw a simple rectangle for demonstration
+    //ctx.drawImage(image, sx, sy, sw, sy, dx, dy, dw, dh); // Draw the player image
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight); // Draw the player image
+    if (gameFrame % staggerFrames === 0) {
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
