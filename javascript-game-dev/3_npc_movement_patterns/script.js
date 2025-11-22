03d80a17b6a89e09d7c23f0dff3bca3e2e1f33ad
QuanTranDoanAnh <td.anhquan@gmail.com>
2025-11-22 11:08:35 +0700

    3_npc_movement_patterns - ennemy 1 movements

--- javascript-game-dev/3_npc_movement_patterns/script.js
+++ javascript-game-dev/3_npc_movement_patterns/script.js
@@ -3,43 +3,35 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
-const numberOfEnemies = 100;
+const numberOfEnemies = 300;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = "enemy1.png";
 let gameFrame = 0;
-/*
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200
-}
-*/
 
 class Enemy {
     constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.speed = Math.random() * 4 - 2;
+        this.image = new Image();
+        this.image.src = "enemy1.png";
+        //this.speed = Math.random() * 4 - 2;
         this.spriteWidth = 293;
         this.spriteHeight = 155;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);
+        this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update() {
-        this.x += this.speed;
-        this.y += this.speed;
+        this.x += Math.random() * 5 - 2.5;
+        this.y += Math.random() * 5 - 2.5;
         // animate sprite
-        if (gameFrame % 2 === 0) {
+        if (gameFrame % this.flapSpeed === 0) {
             this.frame > 4 ? this.frame = 0 : this.frame++;
         }
     }
     draw() {
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, 
             this.x, this.y, this.width, this.height);
     }
 }
